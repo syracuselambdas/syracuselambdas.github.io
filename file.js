c695bc4faf491773e58fc1baad220a23881d9da0
Ricky Diep <rdiep@syr.edu>
2018-10-31 01:04:52 -0500

    need to fix resultion image

--- file.js
+++ file.js
@@ -1,21 +1,4 @@
 
-$(document).ready(function(){   
-
-      //Experience See More / Less button
-      $('.SeeMore').click(function(){
-        var $this = $(this);
-        var $text = this.text
-        console.log($text);
-		$this.toggleClass('SeeMore');
-		if($this.hasClass('SeeMore')){
-            console.log(this)
-			$this.html('Show More   ').append('<i class="fa fa-angle-double-down" aria-hidden="true"></i>');	
-		} else {
-            console.log(this)
-            $this.html('Less   ').append('<i class="fa fa-angle-double-up" aria-hidden="true"></i>');;		
-		}
-	});
- });
 
 
 
