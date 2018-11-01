631be72c067561d3e4377bfcbbead513c4faea7a
rickydiep <35702442+rickydiep@users.noreply.github.com>
2018-11-01 17:11:38 -0500

    sticky nav, needs fix!

--- file.js
+++ file.js
@@ -1,5 +1,16 @@
+$(document).ready(function(){  
+    var feed = new Instafeed ({
+        get:"user",
+        userId:"1531587193",
+        limit: 4,
+        sortBy: "most-recent",
+        resolution: "standard_resolution",
+        accessToken: "1531587193.1677ed0.a24077c78de34f6a9fcd7538c6fb6c98",
+        template: '<div class="image-box "><a href="{{link}}"><div class="instafeed-image-square with-background" style="background-image:url({{image}}); width="85px;"></div></a></div>'
+});
+    feed.run(); 
 
-
-
-
-
+    $('.nav-link').on('click', function(){
+        $('#navbarNav').removeClass('show');
+    });
+});
